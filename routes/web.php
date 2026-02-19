cd6ce41d9256bd225da615bf2cd5c366682ad989
Santiago Jacome <sjacome249@unab.co>
2026-02-19 11:25:01 -0500

    Rutas y controladores basico

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+Route::prefix('product') ->group(function (){
+
+
+Route::get('/product', [ProductController::class, 'index']);
+
+Route::get('/product/create', [ProductController::class, 'create']);
+
+Route::get('/product/{producto}', [ProductController::class, 'show']);
+
 });
