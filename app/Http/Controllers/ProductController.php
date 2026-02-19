51eb47c9e0872fe30c0d6e4bd7256a74c0788a96
Santiago Jacome <sjacome249@unab.co>
2026-02-19 11:28:06 -0500

    Grupo de Rutas

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return view('product.index');
+    }
+
+    public function create(){
+        return view('product.reate');
+    }
+
+    public function show($producto){
+        return view('product.show');
+    }
+}
